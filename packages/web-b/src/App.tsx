357bfe10b72d4b2c7380346f2582a3ef52b4c0ec
Tape <siriwut.pw@gmail.com>
2021-08-25 16:50:19 +0700

    remove yarn lock

--- packages/web-b/src/App.tsx
+++ packages/web-b/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Navbar from "@lib/shared-ui/Navbar"
+import HeroBanner from "@lib/shared-ui/HeroBanner"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <HeroBanner />
+      <h1>Web B</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
