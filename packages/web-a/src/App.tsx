7003d4ca1d8ec97d826736a8a2b643ec47560303
Tape <siriwut.pw@gmail.com>
2021-08-26 10:36:14 +0700

    test prettier 2

--- packages/web-a/src/App.tsx
+++ packages/web-a/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from './logo.svg'
-import './App.css'
+import "./App.css"
 import Navbar from "@lib/shared-ui/Navbar"
 import HeroBanner from "@lib/shared-ui/HeroBanner"
 
