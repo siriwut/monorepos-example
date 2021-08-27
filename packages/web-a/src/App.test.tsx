88ee6687056ac431cf6b9d69b891b5b59ca8320f
Tape <siriwut.pw@gmail.com>
2021-08-27 15:11:05 +0700

    web a title

--- packages/web-a/src/App.test.tsx
+++ packages/web-a/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/Web 555/i)
+  const linkElement = screen.getByText(/Web A/i)
   expect(linkElement).toBeInTheDocument()
 })
